fb0ddd93db6fd6a195f8569532d514c0f61e0f54
Sebastian Springer <88656+sspringer82@users.noreply.github.com>
2024-05-29 16:34:09 +0200

    parent - child

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,13 +1,14 @@
 import './App.css';
 import List from './List';
 import Event from './examples/Event';
+import Parent from './examples/ParentChild';
 import Server from './examples/Server';
 
 // function App() {
 // const App: React.FC = function () {
 // function App(): React.ReactNode {
 const App: React.FC = () => {
-  return <List />;
+  return <Parent />;
 };
 
 export default App;
---
+++ frontend/src/examples/ParentChild.tsx
@@ -0,0 +1,54 @@
+import { useState } from 'react';
+
+type Person = {
+  firstName: string;
+  lastName: string;
+};
+
+const Parent: React.FC = () => {
+  const [state, setState] = useState<Person[]>([
+    { firstName: 'Klaus', lastName: 'Müller' },
+    { firstName: 'Lisa', lastName: 'Meier' },
+  ]);
+
+  function handleRename(oldName: string, newName: string) {
+    setState((prevState) => {
+      return prevState.map((p) => {
+        if (oldName === p.firstName) {
+          return { ...p, firstName: newName };
+        } else {
+          return p;
+        }
+      });
+    });
+  }
+
+  return (
+    <div>
+      <h1>Parent works</h1>
+      {state.map((p) => (
+        <Child person={p} rename={handleRename} />
+      ))}
+    </div>
+  );
+};
+
+export default Parent;
+
+type Props = {
+  person: Person;
+  rename: (oldName: string, newName: string) => void;
+};
+
+const Child: React.FC<Props> = ({ person, rename }) => {
+  function handleClick() {
+    rename(person.firstName, 'Martin');
+  }
+
+  return (
+    <div>
+      child works - {person.firstName}{' '}
+      <button onClick={() => handleClick()}>rename</button>
+    </div>
+  );
+};
