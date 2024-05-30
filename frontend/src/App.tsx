237aea452c8f474ada1cfa6448b8e23a5d8f2886
Sebastian Springer <88656+sspringer82@users.noreply.github.com>
2024-05-30 15:34:45 +0200

    fix it

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,17 +1,11 @@
 import './App.css';
 import List from './List';
-import { ContextProvider } from './examples/Context/Context';
-import Parent from './examples/Context/Parent';
 
 // function App() {
 // const App: React.FC = function () {
 // function App(): React.ReactNode {
 const App: React.FC = () => {
-  return (
-    <ContextProvider>
-      <Parent />
-    </ContextProvider>
-  );
+  return <List />;
 };
 
 export default App;
